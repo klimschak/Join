490e0657a1aa423ea019355eb736821ca9e37879
Emanuel Klimschak <klimschak@gmail.com>
2023-10-05 17:35:13 +0200

    added arrays.js

--- js/add_task.js
+++ js/add_task.js
@@ -1,33 +1,3 @@
-let accounts = [
-      {
-            id: 1,
-            name: 'Siham El-Maimouni',
-            initials: 'SE',
-
-      },
-
-      {
-            id: 2,
-            name: 'Pedro Göntürk ',
-            initials: 'PG',
-
-      },
-
-      {
-            id: 3,
-            name: 'Thorsten Puccini',
-            initials: 'TP',
-
-      },
-];
-
-let tasks = [
-      {
-            assigned: [],
-            id: [],
-      }
-];
-
 
 
 
---
+++ js/arrays.js
@@ -0,0 +1,30 @@
+let accounts = [
+      {
+            id: 1,
+            name: 'Siham El-Maimouni',
+            initials: 'SE',
+
+      },
+
+      {
+            id: 2,
+            name: 'Pedro Göntürk ',
+            initials: 'PG',
+
+      },
+
+      {
+            id: 3,
+            name: 'Thorsten Puccini',
+            initials: 'TP',
+
+      },
+];
+
+let tasks = [
+      {
+            assigned: [],
+            id: [],
+      }
+];
+
