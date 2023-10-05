2c99832632bbaff579523a8ba86156518224cdc9
Emanuel Klimschak <klimschak@gmail.com>
2023-10-05 22:49:22 +0200

    functionality Assigned to Dropdown finalized

--- js/add_task.js
+++ js/add_task.js
@@ -1,29 +1,24 @@
 function displayAccountsInAssignDropdown() {
-
       let assign = document.getElementById(`assign_list`);
-      let badge = document.getElementById('form_assign_badge')
       assign.innerHTML = "";
-      
-      
+
       for (let i = 0; i < accounts.length; i++) {
             const accountId = accounts[i]['id'];
             const assignedIds = tasks[0]['id'];
-            
-             //Falls dem Account i der Task NICHT zugeordnet wurde, soll dies ausgeführt werden
-            if (!assignedIds.includes(accountId)) {
-                  assign.innerHTML += /*html*/`
-                  <li id="assignaccount${i}" onclick="checkIfAssigned(${i})"><div class="form_assign_badge">${accounts[i]['initials']}</div><div class="form_assign_name">${accounts[i]['name']}</div><img id="assigncheck${i}" src="./assets/img/checkbutton_default.svg" alt=""></li>
-                  `;
-                  
+            setStateOfAccountInAssignDropdown (assign, i, accountId, assignedIds);
+      }
+}
 
-
-            }
-            //Falls dem Account i der Task zugeordnet wurde, soll dies ausgeführt werden
-            if (assignedIds.includes(accountId)) {
-                  assign.innerHTML += /*html*/`
-                  <li id="assignaccount${i}" onclick="checkIfAssigned(${i})"><div class="form_assign_badge">${accounts[i]['initials']}</div><div class="form_assign_name">${accounts[i]['name']}</div><img id="assigncheck${i}" src="./assets/img/checkbutton_checked.svg" alt=""></li>
-                  `;
-            }
+function setStateOfAccountInAssignDropdown (assign, i, accountId, assignedIds){
+      if (!assignedIds.includes(accountId)) { //Falls dem Account i der Task NICHT zugeordnet wurde, soll dies ausgeführt werden
+            assign.innerHTML += /*html*/`
+            <li id="assignaccount${i}" onclick="checkIfAssigned(${i})"><div class="form_assign_badge">${accounts[i]['initials']}</div><div class="form_assign_name">${accounts[i]['name']}</div><img id="assigncheck${i}" src="./assets/img/checkbutton_default.svg" alt=""></li>
+            `;
+      }
+      if (assignedIds.includes(accountId)) { //Falls dem Account i der Task zugeordnet wurde, soll dies ausgeführt werden
+            assign.innerHTML += /*html*/`
+            <li id="assignaccount${i}" onclick="checkIfAssigned(${i})"><div class="form_assign_badge">${accounts[i]['initials']}</div><div class="form_assign_name">${accounts[i]['name']}</div><img id="assigncheck${i}" src="./assets/img/checkbutton_checked.svg" alt=""></li>
+            `;
       }
 }
 
@@ -40,7 +35,12 @@ function checkIfAssigned(i) {
       let badge = document.getElementById('form_assign_badge')
       let assignbadge = document.getElementById(`assign_badge${i}`);
       
-      //If Account IS NOT assigned then
+      ifAccountIsNotAssigned (i,accountId, assignedIds, badge)
+      ifAccountIsAssigned (i, index, assignbadge)
+            
+}
+
+function ifAccountIsNotAssigned (i, accountId, assignedIds , badge){
       if (!assignedIds.includes(accountId)) {
             tasks[0]['assigned'].push(accounts[i]['name']);
             tasks[0]['id'].push(accountId);
@@ -49,14 +49,13 @@ function checkIfAssigned(i) {
                   <div id="assign_badge${i}" class="form_assign_badge">${accounts[i]['initials']}</div>
                   `
       }
+}
 
-      //If Account IS assigned then
-      else if (index !== -1) {
+function  ifAccountIsAssigned (i, index, assignbadge) {
+      if (index !== -1) {
             tasks[0]['assigned'].splice(index, 1);
             tasks[0]['id'].splice(index, 1);
             document.getElementById(`assigncheck${i}`).src = './assets/img/checkbutton_default.svg';
             assignbadge.remove();
       }
-
 }
-
