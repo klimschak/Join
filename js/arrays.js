25133a2a288c63540c92ac4b5f70de73c1d8941b
Sahin3892 <75809365+Sahin3892@users.noreply.github.com>
2023-11-06 18:34:47 +0100

    Start with DragnDrop

--- js/arrays.js
+++ js/arrays.js
@@ -1,61 +1,57 @@
 let accounts = [
-      {
-            id: 1,
-            name: 'Siham El-Maimouni',
-            initials: 'SE',
+  {
+    id: 1,
+    name: "Siham El-Maimouni",
+    initials: "SE",
+  },
 
-      },
+  {
+    id: 2,
+    name: "Pedro Göntürk",
+    initials: "PG",
+  },
 
-      {
-            id: 2,
-            name: 'Pedro Göntürk',
-            initials: 'PG',
-
-      },
-
-      {
-            id: 3,
-            name: 'Thorsten Puccini',
-            initials: 'TP',
-
-      },
+  {
+    id: 3,
+    name: "Thorsten Puccini",
+    initials: "TP",
+  },
 ];
 
 let tasks = [
-      { 
-        assigned: ['Pedro Göntürk', 'Thorsten Puccini'],
-        category: ["User Story"],
-        date: [],
-        description: ["Lorem ipsum dolor sit amet, consetetur sadipscing elitr"],
-        id: [],
-        initials: ['PG', 'TP'],
-        priority: ["Urgent"],
-        status: ["To Do"],
-        subtasks: {
-          subtask: ["Task 1", "Task 2", "Task 3", "Task 4", "Task 5"],
-          subtask_id: [0, 1, 2, 3, 4], 
-          completed: [false, true, true, false, false]
-        },
-        title: ["This is a Task Title"],
-        kanban: ["To-Do"],
-      },
+  {
+    assigned: ["Pedro Göntürk", "Thorsten Puccini"],
+    category: ["User Story"],
+    date: [],
+    description: ["Lorem ipsum dolor sit amet, consetetur sadipscing elitr"],
+    id: [],
+    initials: ["PG", "TP"],
+    priority: ["Urgent"],
+    status: ["To Do"],
+    subtasks: {
+      subtask: ["Task 1", "Task 2", "Task 3", "Task 4", "Task 5"],
+      subtask_id: [0, 1, 2, 3, 4],
+      completed: [false, true, true, false, false],
+    },
+    title: ["This is a Task Title"],
+    kanban: ["To-Do"],
+  },
 
-      { 
-            assigned: ['Pedro Göntürk', 'Thorsten Puccini'],
-            category: ["User Story"],
-            date: [],
-            description: ["Lorem ipsum dolor sit amet, consetetur sadipscing elitr"],
-            id: [],
-            initials: ['PG', 'TP'],
-            priority: ["Medium"],
-            status: ["To Do"],
-            subtasks: {
-              subtask: ["Task 1", "Task 2", "Task 3", "Task 4"],
-              subtask_id: [0, 1, 2, 3, 4], 
-              completed: [false, true, true, true,]
-            },
-            title: ["This is another Task"],
-            kanban: ["To-Do"]
-          },
-    ];
-    
+  {
+    assigned: ["Pedro Göntürk", "Thorsten Puccini"],
+    category: ["User Story"],
+    date: [],
+    description: ["Lorem ipsum dolor sit amet, consetetur sadipscing elitr"],
+    id: [],
+    initials: ["PG", "TP"],
+    priority: ["Medium"],
+    status: ["To Do"],
+    subtasks: {
+      subtask: ["Task 1", "Task 2", "Task 3", "Task 4"],
+      subtask_id: [0, 1, 2, 3, 4],
+      completed: [false, true, true, true],
+    },
+    title: ["This is another Task"],
+    kanban: ["To-Do"],
+  },
+];
--- js/board.js
+++ js/board.js
@@ -1,13 +1,13 @@
-function renderAllTasksOnKanban(){
-  let ToDo = document.getElementById('to-do-column');
+function renderAllTasksOnKanban() {
+  let ToDo = document.getElementById("to-do-column");
   ToDo.innerHTML = "";
   for (let i = 0; i < tasks.length; i++) {
     renderTaskCardOnKanban(i);
   }
 }
 
-function renderTaskCardOnKanban(i){
-  let ToDo = document.getElementById('to-do-column');
+function renderTaskCardOnKanban(i) {
+  let ToDo = document.getElementById("to-do-column");
   let category = getKanbanTaskCategory(i);
   let categoryclass = getKanbanTaskCategoryCSSClass(i);
   let title = getKanbanTaskTitle(i);
@@ -16,121 +16,160 @@ function renderTaskCardOnKanban(i){
   let subtaskprogress = getKanbanSubtaskBar(i);
   let subtaskscompleted = getCompletedTasks(i);
   let prio = getPrioForKanban(i);
-  ToDo.innerHTML += htmlTemplateRenderTaskCardOnKanban(i, title, description, category, categoryclass, subtasks, subtaskscompleted, subtaskprogress, prio);
-  getAssignBadgesInitials(i)
+  ToDo.innerHTML += htmlTemplateRenderTaskCardOnKanban(
+    i,
+    title,
+    description,
+    category,
+    categoryclass,
+    subtasks,
+    subtaskscompleted,
+    subtaskprogress,
+    prio
+  );
+  getAssignBadgesInitials(i);
 }
 
 function getKanbanTaskCategory(i) {
   let category = tasks[i].category;
-  return category
+  return category;
 }
 
-function getKanbanTaskCategoryCSSClass(i){
+function getKanbanTaskCategoryCSSClass(i) {
   let category = tasks[i].category;
   let categoryclass;
-  if (category == "User Story"){
-    categoryclass = "user-story"
+  if (category == "User Story") {
+    categoryclass = "user-story";
   }
-  if (category == "Technical Task"){
-    categoryclass = "technical-task"
+  if (category == "Technical Task") {
+    categoryclass = "technical-task";
   }
-  return categoryclass
+  return categoryclass;
 }
 
-function getKanbanTaskTitle(i){ 
+function getKanbanTaskTitle(i) {
   let title = tasks[i].title;
-  return title
+  return title;
 }
 
-function getKanbanTaskDescription(i){
+function getKanbanTaskDescription(i) {
   let description = tasks[i].description;
-  return description
+  return description;
 }
 
-//Anzahl der Subtasks zurückgeben
-function getLengthOfSubtasks(i){
+function getLengthOfSubtasks(i) {
   let task = tasks[i].subtasks.subtask;
   let numberOfTasks = task.length;
-  return numberOfTasks
+  return numberOfTasks;
 }
 
-//Anzahl der abgeschlossenen Subtasks zurückgeben
-function getCompletedTasks(i){
+function getCompletedTasks(i) {
   let completedArray = tasks[i].subtasks.completed;
-  let numberOfTrueValues = completedArray.filter(entity => entity === true).length;
-  return numberOfTrueValues
+  let numberOfTrueValues = completedArray.filter(
+    (entity) => entity === true
+  ).length;
+  return numberOfTrueValues;
 }
 
-function getKanbanSubtaskBar(i){
+function getKanbanSubtaskBar(i) {
   let subtasks = getLengthOfSubtasks(i);
   let completed = getCompletedTasks(i);
-  let subtaskProgress = `${100 / subtasks * completed}%`;
-  return subtaskProgress
+  let subtaskProgress = `${(100 / subtasks) * completed}%`;
+  return subtaskProgress;
 }
 
-function getPrioForKanban(i){
+function getPrioForKanban(i) {
   let prio = tasks[i].priority;
   let prioIcon;
   if (prio == "Urgent") {
-    prioIcon =  `<img src="./assets/img/Prio_alta.svg" alt="">`
+    prioIcon = `<img src="./assets/img/Prio_alta.svg" alt="">`;
   }
   if (prio == "Medium") {
-    prioIcon =  `<img src="./assets/img/Prio_media.svg" alt="">`
+    prioIcon = `<img src="./assets/img/Prio_media.svg" alt="">`;
   }
   if (prio == "Low") {
-    prioIcon =  `<img src="./assets/img/Prio_baja.svg" alt="">`
+    prioIcon = `<img src="./assets/img/Prio_baja.svg" alt="">`;
   }
-  return prioIcon
+  return prioIcon;
 }
 
-//Rendern der Initialien
-function getAssignBadgesInitials(i){
+function getAssignBadgesInitials(i) {
   let initials = tasks[i].initials;
   let assignedTo = document.getElementById(`kanban-assign-to-${i}`);
   assignedTo.innerHTML = ``;
   for (let j = 0; j < initials.length; j++) {
     const initial = initials[j];
-    assignedTo.innerHTML += /*html*/`
+    assignedTo.innerHTML += /*html*/ `
     <div class="kanban-assign-badge">${initial}</div> 
-    `
+    `;
   }
 }
 
-function htmlTemplateRenderTaskCardOnKanban (i, title, description, category, categoryclass, subtasks, subtaskscompleted, subtaskprogress,prio){
-  return /*html*/`
-  <div id="kanban-card-${i}" class="kanban-card">
- 
+function htmlTemplateRenderTaskCardOnKanban(
+  i,
+  title,
+  description,
+  category,
+  categoryclass,
+  subtasks,
+  subtaskscompleted,
+  subtaskprogress,
+  prio
+) {
+  return /*html*/ `
+  <div draggable="true" ondragstart="startDragging(${i})" id="kanban-card-${i}" class="kanban-card">
     <div class="kanban-category-${categoryclass}">
       ${category}
     </div>
- 
     <div class="kanban-title">
-    ${title}
+      ${title}
     </div>
- 
     <div class="kanban-description">
-      ${description}      
+      ${description}
     </div>
- 
     <div class="kanban-subtask">
       <div class="kanban-subtask-progress-container">
         <div class="kanban-subtask-progress-bar" style="width: ${subtaskprogress}">
-      
+        </div>
       </div>
-      </div>
- 
       <div class="kanban-subtask-counter">${subtaskscompleted}/${subtasks} Subtasks</div>
     </div>
- 
     <div class="kanban-badge-prio-container">
       <div class="kanban-assign-prio-container">
         <div class="kanban-assign-badge-container" id="kanban-assign-to-${i}"></div>
         <div class="kanban-prio"></div>
       </div>
- 
       <div class="kanban-prio">${prio}</div>
- 
     </div>
   </div>
- `
- }
+  `;
+}
+
+let currentDraggedElement;
+
+function allowDrop(event) {
+  event.preventDefault();
+}
+
+function startDragging(i) {
+  currentDraggedElement = i;
+}
+
+function dropIntoColumn(ev, columnId) {
+  ev.preventDefault();
+  const i = currentDraggedElement;
+  const kanbanCard = document.getElementById(`kanban-card-${i}`);
+  const targetColumn = document.getElementById(columnId);
+
+  if (kanbanCard && targetColumn) {
+    // Hier wird der gesamte Container verschoben
+    kanbanCard.remove(); // Entferne das Element aus dem ursprünglichen Container
+    targetColumn.appendChild(kanbanCard); // Füge es dem Zielcontainer hinzu
+    moveTo(columnId);
+  }
+}
+
+function moveTo(category) {
+  tasks[currentDraggedElement].category = category; // Ändere "status" zu "category", falls es die richtige Eigenschaft ist.
+  renderAllTasksOnKanban();
+}
