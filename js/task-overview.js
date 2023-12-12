b4dfe0fd2d7a161f063b021f68355e30241e7bff
Emanuel Klimschak <klimschak@gmail.com>
2023-12-12 23:57:47 +0100

    working on task overview css

--- js/task-overview.js
+++ js/task-overview.js
@@ -22,11 +22,12 @@ async function htmlTemplateLoadTaskOverview(task) {
 
       return /*html*/`
             <div id="overview-container" class="overview-container">
-                  <div ><div  id="overview-category">${task.category}</div></div>
+                  <div  id="overview-category">${task.category}</div>
                   <div class="overview-title">${task.title}</div>
-                  <div class="overview-date">Due Date: ${task.date}</div>
-                  <div class="overview-priority">Priority: ${task.priority}</div>
-                  <div id="overview-assigned" class="overview-assigned" onload=""></div>
+                  <div class="overview-description">${task.description}</div>
+                  <div class="overview-date"><div>Due Date:</div><div>${task.date}</div></div>
+                  <div class="overview-priority"><div>Priority:</div><div>${task.priority}</div></div>
+                  <div id="overview-assigned" class="overview-assigned" onload=""><div>Assigned to:</div></div>
             </div>
       
       `
@@ -41,7 +42,7 @@ async function loadOverviewAssigned(task){
             const initials = task.initials[i];
             const name = task.assigned[i];
             div.innerHTML += /*html*/`
-            <div>${initials} ${name}</div>
+            <div class="overview-assign-badge-container"><div class="overview-assign-badge">${initials}</div> <div>${name}</div></div>
             `
 
 
