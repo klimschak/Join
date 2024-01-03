9cdaa60aac5cd49d9f1b1336c2bb02c84967c74c
Emanuel Klimschak <107833744+klimschak@users.noreply.github.com>
2024-01-03 22:17:32 +0100

    started with edit task function

--- js/edit-task.js
+++ js/edit-task.js
@@ -1,21 +1,15 @@
+
+// Die Funktion übernimmt die TaskID mit der das dazugehörige Array angesteuert werden kann
+// Die Funktion bildet den Start, um Tasks zu editieren
 async function loadTaskEdit(taskID) {
       let overlay = document.getElementById('edit-task-overlay');
       overlay.classList.remove('d-none');
       //addEventlistenerToSubtaskField ()
-      await initEditTask(taskID)
+      console.log('Task ID:', taskID);
+      task = tasks[taskID];
 
     }
 
-
-    async function initEditTask(taskID){
-      //await loadTasksToAddTasksFromRemoteStorage();
-      //addEventlistenerToSubtaskField ()
-      createInitialsFromName();
-      console.log('Task ID:', taskID);
-
-
-      
-      
-      
-      
-}
+// Funktion die die einzelnen Formularelemente rendert
+// Zuerst sollen die Formularelemente im Iniitalzustand erstellt werden
+// Funktion die die einzelnen Formularelemente mit den Inhalten aus dem Array befüllt
