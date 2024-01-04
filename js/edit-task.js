d2cddf42df4a4726e75cf92c62fe28c50cf4a6d2
Emanuel Klimschak <107833744+klimschak@users.noreply.github.com>
2024-01-04 21:00:21 +0100

    added category dropdown to edit task

--- js/edit-task.js
+++ js/edit-task.js
@@ -7,10 +7,47 @@ async function loadTaskEdit(taskID) {
       document.getElementById('task-overview-overlay').classList.add('d-none')
       //addEventlistenerToSubtaskField ()
       console.log('Task ID:', taskID);
-      task = tasks[taskID];
+      let task = tasks[taskID];
+      renderEditTaskBackground()
+      loadCategoryInEditTask(taskID)
+      
 
     }
 
 // Funktion die die einzelnen Formularelemente rendert
 // Zuerst sollen die Formularelemente im Iniitalzustand erstellt werden
 // Funktion die die einzelnen Formularelemente mit den Inhalten aus dem Array befüllt
+
+function renderEditTaskBackground(){
+  let overlay = document.getElementById('edit-task-overlay');
+  overlay.innerHTML = /*html*/`
+    <div id="edit-task-container" class="edit-task-container"></div>
+
+    `
+}
+
+function loadCategoryInEditTask(taskID){
+  let container = document.getElementById('edit-task-container');
+  let task = tasks[taskID].category;
+  container.innerHTML = /*html*/`
+                        <div class="category_container">
+                        <div class="category_label">
+                              <h4>Category<span class="required">*</span></h4>
+                        </div>
+
+                        <div class="category_field_dropdown_container pointer" id="category_field_dropdown_container">
+                              <div class="category_field" onclick="toggleCategoryDropdown()">
+                                    <p id="category_field_title" class="category_field_title">${task}</p>
+                                    <img src="./assets/img/arrow_drop_downaa.svg" alt="">
+                              </div>
+                              <ul id="category_dropdown" class="category_dropdown">
+
+                              </ul>
+                        </div>
+
+                        <div class="form_input_notice">
+                              <p class="d-none">This field is required</p>
+                        </div>
+                  </div>
+    `
+}
