a97ee7c0143564b48946f7ebc5832b23f8d5860b
Sahin <sahin.duruoez@gmx.de>
2024-01-26 10:11:30 +0100

    added footer in contact

--- js/contact.js
+++ js/contact.js
@@ -21,6 +21,21 @@ let userContacts = [
   },
 ];
 
+async function includeHTML() {
+  let includeElements = document.querySelectorAll('[w3-include-html]');
+  for (let i = 0; i < includeElements.length; i++) {
+      const element = includeElements[i];
+      file = element.getAttribute("w3-include-html"); 
+      let resp = await fetch(file);
+      if (resp.ok) {
+          element.innerHTML = await resp.text();
+      } else {
+          element.innerHTML = 'Page not found';
+      }
+  }
+  changeClassToActive();
+}
+
 async function renderContacts() {
   console.log("Rendering contacts...");
   const contactsContainer = document.getElementById("contacts");
